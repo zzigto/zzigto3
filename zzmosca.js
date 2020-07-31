f135974ca36e43c84cd81302f66e62825f3682b5
Mario Rolle <zzigto.iot@gmail.com>
2020-07-31 19:09:02 +0200

    zzigto3

--- zzmosca.js
+++ zzmosca.js
@@ -1,15 +1,24 @@
 var mosca = require('mosca')
 var fs = require('fs');
+//var retus = require("retus");
 
 var GLOBAL_MOSCA=null;
 
 class zzmosca
 {
-    constructor(zzweb, superuser, superpwd) 
-    { 
+    constructor(zzweb, superuser, superpwd/*, localconfig*/) 
+    {
 		GLOBAL_MOSCA=this;
 		this.status=false;
+		/*this.localconfig=localconfig;
+		if (!zzweb && localconfig)
+		{
+			//ESECUZIONE SEPARATA MQTT SERVER
+			init_local_config();
+		}
+		else*/
 		this.zzweb=zzweb;
+		
 		this.superuser=superuser;
 		this.superpwd=superpwd;
 		this.connections={};
@@ -39,6 +48,8 @@ class zzmosca
 			}
 		};
 		
+		
+
 		console.log("MQTT Server port TCP", this.zzweb.jsystem.mqttport);
 		console.log("MQTT Server port WS", this.zzweb.jsystem.mqttwsport);
 		console.log("MQTT Server port TCP SSL", this.zzweb.jsystem.mqttsslport);
@@ -60,46 +71,46 @@ class zzmosca
 			catch(e)
 			{
 				//console.log("ready", e);
-				GLOBAL_MOSCA.zzweb.utils.log("Mosca error:", e.message);
+				console.log("Mosca error:", e.message);
 
 			}
 		});
 		
 		this.server.authenticate = function (client, username, password, callback) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.1");
+			console.log("zzmosca.authenticate.1");
 			try
 			{
 				if (!client.id)
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.failed.1");
+					console.log("zzmosca.authenticate.failed.1");
 					callback(null, false);
 					return;
 				}
 					
 				var su=false;
 				var spassword=password.toString('utf8');
-				GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate", client.id, username, spassword);
+				console.log("zzmosca.authenticate", client.id, username, spassword);
 				
 				if (GLOBAL_MOSCA.superuser==username && GLOBAL_MOSCA.superpwd==spassword)
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.superuser");
+					console.log("zzmosca.authenticate.superuser");
 					su=true;
 				}
 				else
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.step.1", username, spassword);
+					console.log("zzmosca.authenticate.step.1", username, spassword);
 					var jret=GLOBAL_MOSCA.zzweb.zzaccount.get_account(username, spassword);
 					if (jret.error)
 					{
 						//delete this.connections[client.id];
-						GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.failed.2", username, spassword);
+						console.log("zzmosca.authenticate.failed.2", username, spassword);
 						callback(null, false);
 						return;
 					}
-					GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.ok", username, spassword);
+					console.log("zzmosca.authenticate.ok", username, spassword);
 				}
-				GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authenticate.ok", client.id, username, spassword);
+				console.log("zzmosca.authenticate.ok", client.id, username, spassword);
 				
 				GLOBAL_MOSCA.connections[client.id]={};
 				GLOBAL_MOSCA.connections[client.id].user=username;
@@ -109,144 +120,144 @@ class zzmosca
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("authenticate.error", e.message);
+				console.log("authenticate.error", e.message);
 			}
 		};	
 		
 		this.server.authorizePublish=function(client, topic, payload, callback) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authorizePublish.1");
+			console.log("zzmosca.authorizePublish.1");
 			try
 			{
 				if (GLOBAL_MOSCA.connections[client.id].su || topic.startsWith(GLOBAL_MOSCA.connections[client.id].user))
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("Mosca authorizePublish", client.id, topic);
+					console.log("Mosca authorizePublish", client.id, topic);
 					callback(null, true);
 					return;
 				}
-				GLOBAL_MOSCA.zzweb.utils.log("Mosca authorizePublish.fail", client.id);
+				console.log("Mosca authorizePublish.fail", client.id);
 				callback(null, false);
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("authorizePublish.error", e.message);
+				console.log("authorizePublish.error", e.message);
 			}
 		};
 		this.server.authorizeSubscribe=function(client, topic, callback) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.authorizeSubscribe.1");
+			console.log("zzmosca.authorizeSubscribe.1");
 			try
 			{
 				//console.log("Mosca authorizeSubscribe.connections", GLOBAL_MOSCA.connections);
 				if (GLOBAL_MOSCA.connections[client.id].su || topic.startsWith(GLOBAL_MOSCA.connections[client.id].user))
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("Mosca authorizeSubscribe.ok", client.id, topic);
+					console.log("Mosca authorizeSubscribe.ok", client.id, topic);
 					callback(null, true);
 					return;
 				}
-				GLOBAL_MOSCA.zzweb.utils.log("Mosca authorizeSubscribe.fail", client.id, topic);
+				console.log("Mosca authorizeSubscribe.fail", client.id, topic);
 				callback(null, false);
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("authorizeSubscribe.error", e.message);
+				console.log("authorizeSubscribe.error", e.message);
 			}
 		};
 		
 		this.server.on('clientConnected', function(client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.clientConnected.1");
+			console.log("zzmosca.clientConnected.1");
 			try
 			{
-				GLOBAL_MOSCA.zzweb.utils.log('client connected');
+				console.log('client connected');
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("clientConnected.error", e.message);
+				console.log("clientConnected.error", e.message);
 			}
 		});
 
 		this.server.on('published', function(packet, client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("published.1");
+			console.log("published.1");
 			var message="";
 			try
 			{
 				if (packet.topic.endsWith("/out"))
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("published.2");
+					console.log("published.2");
 					message=packet.payload.toString('utf8');
 					
 					var jmessage=JSON.parse(message);
 					if (jmessage.type && jmessage.type=="data" && jmessage.T && jmessage.SV!=0)
 					{
-						GLOBAL_MOSCA.zzweb.utils.log("published.2.1");
+						console.log("published.2.1");
 						GLOBAL_MOSCA.saveDataM(packet.topic, jmessage);
 					}
 					else
-						GLOBAL_MOSCA.zzweb.utils.log("published.2.2");
+						console.log("published.2.2");
 				}
 				else
-					GLOBAL_MOSCA.zzweb.utils.log("published.3");
+					console.log("published.3");
 				
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("published.error", e.message);
+				console.log("published.error", e.message);
 			}
 		});
 
 		this.server.on('subscribed', function(topic, client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.subscribed.1");
+			console.log("zzmosca.subscribed.1");
 			try
 			{
-				GLOBAL_MOSCA.zzweb.utils.log('subscribed : ', topic);
+				console.log('subscribed : ', topic);
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("subscribed.error", e.message);
+				console.log("subscribed.error", e.message);
 			}
 		});
 
 		this.server.on('unsubscribed', function(topic, client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.unsubscribed.1");
+			console.log("zzmosca.unsubscribed.1");
 			try
 			{
-				GLOBAL_MOSCA.zzweb.utils.log('unsubscribed : ', topic);
+				console.log('unsubscribed : ', topic);
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("unsubscribed.error", e.message);
+				console.log("unsubscribed.error", e.message);
 			}
 		});
 
 		this.server.on('clientDisconnecting', function(client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.clientDisconnecting.1");
+			console.log("zzmosca.clientDisconnecting.1");
 			try
 			{
-				GLOBAL_MOSCA.zzweb.utils.log('clientDisconnecting : ', client.id);
+				console.log('clientDisconnecting : ', client.id);
 				delete GLOBAL_MOSCA.connections[client.id];
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("clientDisconnecting.error", e.message);
+				console.log("clientDisconnecting.error", e.message);
 			}
 		});
 
 		this.server.on('clientDisconnected', function(client) 
 		{
-			GLOBAL_MOSCA.zzweb.utils.log("zzmosca.clientDisconnected.1");
+			console.log("zzmosca.clientDisconnected.1");
 			try
 			{
-				GLOBAL_MOSCA.zzweb.utils.log('clientDisconnected : ', client.id);
+				console.log('clientDisconnected : ', client.id);
 				delete GLOBAL_MOSCA.connections[client.id];
 			}
 			catch(e)
 			{
-				GLOBAL_MOSCA.zzweb.utils.log("clientDisconnected.error", e.message);
+				console.log("clientDisconnected.error", e.message);
 			}
 		});		
     }
@@ -266,7 +277,7 @@ class zzmosca
         }
         catch(e)
         {
-			GLOBAL_MOSCA.zzweb.utils.log('zzmqtt.publish.exception.error', e.message);
+			console.log('zzmqtt.publish.exception.error', e.message);
         }        
 		return jret;
     }
@@ -297,7 +308,7 @@ class zzmosca
 		
     saveDataM(topic, jmessage) 
 	{
-		GLOBAL_MOSCA.zzweb.utils.log("saveDataM.1");
+		console.log("saveDataM.1");
 		
 		var items=GLOBAL_MOSCA.splitTopic(topic);
 		if (items.root && items.device)
@@ -407,7 +418,7 @@ class zzmosca
 							}
 							catch(e)
 							{
-								GLOBAL_MOSCA.zzweb.utils.log("zzmosca.error.2:", e.message);
+								console.log("zzmosca.error.2:", e.message);
 							}
 								
 							try
@@ -419,8 +430,8 @@ class zzmosca
 										//if (!jdata[H][prop])
 										//	jdata[H][prop]={};
 										
-										jdata[H][prop].N=jmessage.value[prop].M;
-										jdata[H][prop].TN=jmessage.value[prop].TM;
+										jdata[H][prop].M=jmessage.value[prop].M;
+										jdata[H][prop].TM=jmessage.value[prop].TM;
 									}
 								}
 								//else
@@ -428,7 +439,7 @@ class zzmosca
 							}
 							catch(e)
 							{
-								GLOBAL_MOSCA.zzweb.utils.log("zzmosca.error.3:", e.message);
+								console.log("zzmosca.error.3:", e.message);
 							}
 						}
 						
@@ -469,7 +480,7 @@ class zzmosca
 							}
 							catch(e)
 							{
-								GLOBAL_MOSCA.zzweb.utils.log("zzmosca.error.1.error:", e.message);
+								console.log("zzmosca.error.1.error:", e.message);
 							}						
 						}
 						//jdata[H]=jmessage.value;
@@ -479,7 +490,7 @@ class zzmosca
 				}
 				else
 				{
-					GLOBAL_MOSCA.zzweb.utils.log("INVALID JSON");
+					console.log("INVALID JSON");
 				}
 			}
 			
@@ -487,6 +498,41 @@ class zzmosca
 			//console.log("DATA:", jmessage, jmessage.T, H, D);
 		}
 	}
+	
+	
+	/*init_local_config()
+	{
+		try
+		{
+			if (this.localconfig && this.localconfig.home_directory && this.localconfig.zzwebUrl)
+			{
+				this.zzweb={};
+				this.zzweb.home_directory=this.localconfig.home_directory;
+				this.zzweb.zzwebUrl=this.localconfig.zzwebUrl;
+				
+				var attributes=fs.readFileSync(this.zzweb.home_directory+"/system/system.json", {encoding:'utf8', flag:'r'});
+				this.zzweb.jsystem=JSON.parse(attributes);
+			}
+		}
+		catch(e)
+		{
+			console.log("zzmosca.httpRequest.error:", e.message);
+		}						
+	}
+	
+	httpRequest()
+	{
+		try
+		{
+			var body = retus(url);
+			return body.body;
+		}
+		catch(e)
+		{
+			console.log("zzmosca.httpRequest.error:", e.message);
+		}						
+	}*/
+	
 }
 
 exports.zzmosca=zzmosca;
